90904670878795a5426a53c74f747af96f3d47b4
jEzEk <jezek@chicki.sk>
2025-06-14 00:58:41 +0200

    prefs: add defaults reset

--- prefs.js
+++ prefs.js
@@ -11,11 +11,13 @@ export default class Prefs extends ExtensionPreferences {
     fillPreferencesWindow(window) {
         const page = new Adw.PreferencesPage();
         const group = new Adw.PreferencesGroup({ title: 'Modifier Mapping' });
+        const entries = {};
         const addMapEntry = (key, title) => {
             const entry = new Gtk.Entry({ text: this.settings.get_string(key) });
             entry.connect('changed', () => {
                 this.settings.set_string(key, entry.text);
             });
+            entries[key] = entry;
             const row = new Adw.ActionRow({ title });
             row.add_suffix(entry);
             row.activatable_widget = entry;
@@ -30,6 +32,32 @@ export default class Prefs extends ExtensionPreferences {
         addMapEntry('mod3-symbol', 'Mod3');
         addMapEntry('mod4-symbol', 'Mod4 / Super');
         addMapEntry('mod5-symbol', 'Mod5');
+
+        const defaultsKeys = ['shift-symbol', 'caps-symbol', 'control-symbol',
+            'mod1-symbol', 'mod2-symbol', 'mod3-symbol', 'mod4-symbol', 'mod5-symbol'];
+        const pcDefaults = ['⇧', '⇪', '⌃', '⎇', '⇭', '⇳', '❖', '⎈'];
+        const macDefaults = ['⇧', '⇬', '⋀', '⌥', '①', '◆', '⌘', '⎇'];
+
+        const resetRow = new Adw.ActionRow({ title: 'Reset defaults' });
+        const pcButton = new Gtk.Button({ label: 'PC' });
+        const macButton = new Gtk.Button({ label: 'Mac' });
+        pcButton.connect('clicked', () => {
+            defaultsKeys.forEach((k, i) => {
+                this.settings.set_string(k, pcDefaults[i]);
+                if (entries[k])
+                    entries[k].text = pcDefaults[i];
+            });
+        });
+        macButton.connect('clicked', () => {
+            defaultsKeys.forEach((k, i) => {
+                this.settings.set_string(k, macDefaults[i]);
+                if (entries[k])
+                    entries[k].text = macDefaults[i];
+            });
+        });
+        resetRow.add_suffix(pcButton);
+        resetRow.add_suffix(macButton);
+        group.add(resetRow);
 
         page.add(group);
 
